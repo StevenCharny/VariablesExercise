35cf39f7fe89cd03e3d6587025191ec96a7e05e5
StevenCharny <steven@charny.co>
2021-09-22 02:04:02 -0500

    Submitting final assignment file

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1
+// const
+// 2
+let num =20;
+console.log(num)
+// 3
+num = num - 5;
+console.log(num)
+// 4
+num = num + 7;
+console.log(num)
+// 5
+console.log(num - 1)
+// 6
+num = 'This is not a number'
+// 7
+let blank;
+// 8
+console.log(blank)
+// 9
+blank = null;
+console.log(blank)
+
+//bonus
+// 1
+var quarter = .25, dime = .10, nickle = .05, penny = .01;
+// 2
+console.log(quarter, dime, nickle, penny)
+// 3
+let letters = 'qwertyuiop'
+// 4
+console.log(letters[letters.length-1])
